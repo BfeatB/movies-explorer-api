fada7eff601699ffe72f5c9dd253d6157ddfd085
Valentina Karpova <bfeatb@mail.ru>
2022-02-03 17:49:27 +0000

    feat: adjust rate limit

--- utils/rate-limiter.js
+++ utils/rate-limiter.js
@@ -1,9 +1,9 @@
 const rateLimit = require('express-rate-limit');
 
-// 100 requests per minute
+// 1 requests per second
 const limiter = rateLimit({
   windowMs: 60 * 1000,
-  max: 100,
+  max: 60,
   standardHeaders: true,
   legacyHeaders: false,
 });
